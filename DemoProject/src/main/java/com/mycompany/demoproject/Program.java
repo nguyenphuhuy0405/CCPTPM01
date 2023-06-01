37b56cdc76985bca9d8434b13d17140eab389afb
nguyenphuhuy0405 <89263194+nguyenphuhuy0405@users.noreply.github.com>
2023-06-01 11:09:08 +0700

    Add files via upload

---
+++ DemoProject/src/main/java/com/mycompany/demoproject/Program.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.demoproject;
+
+/**
+ *
+ * @author IT Supporter
+ */
+public class Program {
+    public static void main(String[] args) {
+        SinhVien sv = new SinhVien(1 , "Duc0", "Ha noi");
+        
+        System.out.println("Sinh vien :" + sv);
+    }
+    
+}
---
+++ DemoProject/src/main/java/com/mycompany/demoproject/SinhVien.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.demoproject;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author IT Supporter
+ */
+public class SinhVien {
+    private int id;
+    private String name;
+    private String diaChi;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(int id, String name, String diaChi) {
+        this.id = id;
+        this.name = name;
+        this.diaChi = diaChi;
+    }
+    
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    @Override
+    public String toString() {
+        return "SinhVien{" + "id=" + id + ", name=" + name + ", diaChi=" + diaChi + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + this.id;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + Objects.hashCode(this.diaChi);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SinhVien other = (SinhVien) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.diaChi, other.diaChi)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
